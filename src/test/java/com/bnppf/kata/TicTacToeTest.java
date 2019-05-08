5e66123a353a76cd5581caafee31225d60a4e0a6
2019_SOW_DEV_016 <2019SowDev016@gmail.com>
2019-05-08 22:07:50 +0200

    Added the maven jUnit dependency required for junit testing

--- src/test/java/com/bnppf/kata/TicTacToeTest.java
+++ src/test/java/com/bnppf/kata/TicTacToeTest.java
@@ -1,5 +1,14 @@
 package com.bnppf.kata;
 
+import org.junit.Before;
+
 public class TicTacToeTest {
     private TicTacToe ticTacToe;
+
+    @Before
+    public void init(){
+        ticTacToe = new TicTacToe();
+    }
+
+
 }
