1e3cd968d30cf8d2a290946b067e97fd4f470ae9
2019_SOW_DEV_016 <2019SowDev016@gmail.com>
2019-05-08 22:36:38 +0200

    Added the test case for the first move by PlayerX to fill a position with X

--- src/main/java/com/bnppf/kata/TicTacToe.java
+++ src/main/java/com/bnppf/kata/TicTacToe.java
@@ -1,4 +1,17 @@
 package com.bnppf.kata;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TicTacToe {
+
+    private Map<Integer, String> positionBlockStatusMap = new HashMap<Integer, String>();
+
+    protected void blockPosition(int position, String playerName) {
+        positionBlockStatusMap.put(position, playerName);
+    }
+
+    public Map<Integer, String> getPositionStatus() {
+        return positionBlockStatusMap;
+    }
 }
--- src/test/java/com/bnppf/kata/TicTacToeTest.java
+++ src/test/java/com/bnppf/kata/TicTacToeTest.java
@@ -1,6 +1,10 @@
 package com.bnppf.kata;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
 
 public class TicTacToeTest {
     private TicTacToe ticTacToe;
@@ -8,6 +12,16 @@ public class TicTacToeTest {
     @Before
     public void init(){
         ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void When_playerXplacesAtFirstPosition_Expect_FirstPositionIsNotAvailable(){
+        ticTacToe.blockPosition(1, "playerX");
+
+        Map<Integer, String> filledBlockStatusMap = ticTacToe.getPositionStatus();
+        String playerName =  filledBlockStatusMap.get(1);
+
+        Assert.assertEquals("playerX", playerName);
     }
 
 
