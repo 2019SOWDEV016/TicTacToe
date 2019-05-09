6f4a92c1b34193374d939dcb1422abc084f11b3d
2019_SOW_DEV_016 <2019SowDev016@gmail.com>
2019-05-09 23:42:42 +0200

    Checking the validation of the postions marked

--- src/main/java/com/bnppf/kata/TicTacToeGame.java
+++ src/main/java/com/bnppf/kata/TicTacToeGame.java
@@ -1,11 +1,13 @@
 package com.bnppf.kata;
 
+import java.util.Map;
+
 public class TicTacToeGame {
     public static void main(String[] args) {
 
     }
 
-    void printBoard() {
+    public static void printBoard() {
         System.out.println("+---+---+---+");
         System.out.println("| " + "1" + " | " + "2" + " | " + "3" + " |");
         System.out.println("+---+---+---+");
@@ -13,5 +15,41 @@ public class TicTacToeGame {
         System.out.println("+---+---+---+");
         System.out.println("| " + "7" + " | " + "8" + " | " + "9" + " |");
         System.out.println("+---+---+---+");
+    }
+
+    private static void printBoard(TicTacToe ticTacToe) {
+        Map<Integer, String> positionBlockStatusMap = ticTacToe.getBoardStatus();
+        String positinOne = ( null != positionBlockStatusMap.get(1) ? positionBlockStatusMap.get(1) : " ");
+        String positinTwo = ( null != positionBlockStatusMap.get(2) ? positionBlockStatusMap.get(2) : " ");
+        String positinThree = ( null != positionBlockStatusMap.get(3) ? positionBlockStatusMap.get(3) : " ");
+        String positinFour = ( null != positionBlockStatusMap.get(4) ? positionBlockStatusMap.get(4) : " ");
+        String positinFive = ( null != positionBlockStatusMap.get(5) ? positionBlockStatusMap.get(5) : " ");
+        String positinSix = ( null != positionBlockStatusMap.get(6) ? positionBlockStatusMap.get(6) : " ");
+        String positinSeven = ( null != positionBlockStatusMap.get(7) ? positionBlockStatusMap.get(7) : " ");
+        String positinEight = ( null != positionBlockStatusMap.get(8) ? positionBlockStatusMap.get(8) : " ");
+        String positinoneNine = ( null != positionBlockStatusMap.get(9) ? positionBlockStatusMap.get(9) : " ");
+
+        System.out.println("+---+---+---+");
+        System.out.println("| " + positinOne + " | " + positinTwo + " | " + positinThree + " |");
+        System.out.println("+---+---+---+");
+        System.out.println("| " + positinFour + " | " + positinFive + " | " + positinSix + " |");
+        System.out.println("+---+---+---+");
+        System.out.println("| " + positinSeven + " | " + positinEight + " | " + positinoneNine + " |");
+        System.out.println("+---+---+---+");
+    }
+
+    public void placeMark(TicTacToe ticTacToe, int position, String mark) {
+        if(ticTacToe.blockPosition(position, mark)){
+            String gameStatus = ticTacToe.checkGameStatus();
+            if("not finished".equals(gameStatus)){
+                printBoard(ticTacToe);
+            } else{
+                printBoard(ticTacToe);
+                System.out.println("Final Game Result is ----> "+gameStatus);
+            }
+        } else{
+            System.out.println("\n Attention! Please choose a valid position to place "+mark+"\n");
+            printBoard(ticTacToe);
+        }
     }
 }
--- src/test/java/com/bnppf/kata/TicTacToeGameTest.java
+++ src/test/java/com/bnppf/kata/TicTacToeGameTest.java
@@ -21,4 +21,28 @@ public class TicTacToeGameTest {
     public void test_When_NoPositions_Expect_EmptyGameBoard(){
         ticTacToeGame.printBoard();
     }
+
+    @Test
+    public void test_When_PlayerXPostionAtFirstPosition_Expect_XinFirstPosition(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToeGame.placeMark(ticTacToe,1, "X");
+    }
+
+    @Test
+    public void test_When_TryToPositionWhichIsFilled_Expect_Warning(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToeGame.placeMark(ticTacToe, 1, "X");
+        ticTacToeGame.placeMark(ticTacToe, 1, "O");
+    }
+
+    @Test
+    public void test_When_PositionsMatchesWinningPattern_Expect_Win(){
+        TicTacToe ticTacToe = new TicTacToe();
+
+        ticTacToeGame.placeMark(ticTacToe, 1, "X");
+        ticTacToeGame.placeMark(ticTacToe, 7, "O");
+        ticTacToeGame.placeMark(ticTacToe, 2, "X");
+        ticTacToeGame.placeMark(ticTacToe, 5, "0");
+        ticTacToeGame.placeMark(ticTacToe, 3, "X");
+    }
 }
