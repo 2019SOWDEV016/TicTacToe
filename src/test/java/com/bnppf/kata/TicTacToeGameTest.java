6cbed582aca66608bb52cd813f587033950cfa3c
2019_SOW_DEV_016 <2019SowDev016@gmail.com>
2019-05-10 12:17:19 +0200

    Refactoring the test cases

--- src/test/java/com/bnppf/kata/TicTacToeGameTest.java
+++ src/test/java/com/bnppf/kata/TicTacToeGameTest.java
@@ -51,14 +51,14 @@ public class TicTacToeGameTest {
     @Test
     public void test_When_StartGameWithGivenInput_Expect_ShowPlayerXAsWinner(){
         System.setIn(new ByteArrayInputStream("1\n2\n3\n3\n4\n5\n6\n7\n8\n9".getBytes()));
-
+        Scanner scanner = new Scanner(System.in);
         ticTacToeGame.startGame(scanner);
     }
 
     @Test
     public void test_When_StartGameWithGivenInput_Expect_ShowPlayerOAsWinner(){
         System.setIn(new ByteArrayInputStream("1\n4\n2\n5\n7\n6".getBytes()));
-
+        Scanner scanner = new Scanner(System.in);
         ticTacToeGame.startGame(scanner);
     }
 
